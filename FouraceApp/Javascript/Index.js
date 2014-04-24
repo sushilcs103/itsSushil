1a42942cf716501b44b09a79f1df7a5ee5fc1d38
sushil kumar <sushilcs103@gmail.com>
2014-04-24 09:01:42 -0500

    Changes made by sushil kumar (sushilcs103@gmail.com).

---
+++ FouraceApp/Javascript/Index.js
@@ -0,0 +1,12 @@
+function facebook(){
+   var ref = window.open(encodeURI('https://www.facebook.com'), '_blank', 'location=yes');
+}
+function google(){
+   var ref = window.open(encodeURI('https://www.google.com'), '_blank', 'location=yes');
+}
+function twitter(){
+   var ref = window.open(encodeURI('https://www.twitter.com'), '_blank', 'location=yes');
+}
+function youtube(){
+   var ref = window.open(encodeURI('https://www.Youtube.com'), '_blank', 'location=yes');
+}
