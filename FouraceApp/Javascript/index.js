ae9f64f8c5443ea775e5bbf61e769366e9d17f90
Nagaraj M <nagarajm468@gmail.com>
2014-04-25 06:53:28 -0500

    Changes made by Nagaraj M (nagarajm468@gmail.com).

--- FouraceApp/Javascript/index.js
+++ FouraceApp/Javascript/index.js
@@ -1,41 +1,28 @@
 function facebook()
 {
-  var ref = window.open(encodeURI('https://www.facebook.com'), '_blank', 'location=yes');
+   window.open(encodeURI('https://www.facebook.com'), '_blank', 'location=yes');
 }
  
 function google()
 {
    
-    var ref = window.open(encodeURI('https://www.google.com'), '_blank', 'location=yes');  
+     window.open(encodeURI('https://www.google.com'), '_blank', 'location=yes');  
     
   }
 
 function twitter()
 {
     
-  var ref = window.open(encodeURI('https://www.twitter.com'), '_blank', 'location=yes');  
+  window.open(encodeURI('https://www.twitter.com'), '_blank', 'location=yes');  
     
 }
 
 function youtube()
 {
     
-  var ref = window.open(encodeURI('https://www.youtube.com'), '_blank', 'location=yes');  
+ window.open(encodeURI('https://www.youtube.com'), '_blank', 'location=yes');  
     
 }
-
-function sendmail()
-{
-      alert('hi')
- 
-}
-
-
-
-
-
-
-
 
 
 
