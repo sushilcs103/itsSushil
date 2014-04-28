92dab6b457b28d24a4dcb0932febe5747cd1df00
Nagaraj M <nagarajm468@gmail.com>
2014-04-28 04:02:06 -0500

    Changes made by Nagaraj M (nagarajm468@gmail.com).

--- FouraceApp/Javascript/index.js
+++ FouraceApp/Javascript/index.js
@@ -29,3 +29,12 @@ function sendmail()
       alert('hi')
  
 }
+
+	
+
+$( document ).bind( 'mobileinit', function(){
+  $.mobile.loader.prototype.options.text = "loading";
+  $.mobile.loader.prototype.options.textVisible = false;
+  $.mobile.loader.prototype.options.theme = "a";
+  $.mobile.loader.prototype.options.html = "";
+});
